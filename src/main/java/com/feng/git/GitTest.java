e9a9684f0817a7912e9b0652c8ba322538c3f8a3
feng <1144257438@qq.com>
2022-11-20 17:06:47 +0800

    first commit

---
+++ src/main/java/com/feng/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.feng.git;
+
+/**
+ * @description:
+ * @author: SorryMaker
+ * @create: 2022-11-20 16:53
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,git!");
+    }
+}
