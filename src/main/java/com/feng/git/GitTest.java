ae1c9e5f98eaa8d20ebee71cf4f05478a6eb0dee
feng <1144257438@qq.com>
2022-11-21 16:38:40 +0800

    push commit
    Signed-off-by: feng <1144257438@qq.com>

--- src/main/java/com/feng/git/GitTest.java
+++ src/main/java/com/feng/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("Hello git4");
         System.out.println("Master Test!");
         System.out.println("Hot-fix test!");
+        System.out.println("Push test!");
 
     }
 }
